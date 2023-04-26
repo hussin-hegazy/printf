a682dc6e9fa4519360ca34b3b0cc004a81c8e124
hussin-hegazy <hussinhegazy350@gmail.com>
2023-04-26 08:26:04 -0700

    c

--- _printf.c
+++ _printf.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-#include <stdarg.h>
+#include <stdlib.h>
+#include "holberton.h"
+
 /**
  * _printf - function my printf
  * @format: string whit format to print
@@ -8,8 +10,15 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list as;
-    va_start(as, format);
-    vprintf(format, as);
-    va_end(as);
+	va_list args;
+	int length = 0;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	length = _print_format(format, args);
+	va_end(args);
+	return (length);
 }
